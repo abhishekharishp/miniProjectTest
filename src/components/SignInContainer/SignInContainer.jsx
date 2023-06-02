f6caf4b47a8e41cfd4e7a9e467340c2c14bb5ef1
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-06-02 18:20:54 +0530

    changed login from div to form

--- src/components/SignInContainer/SignInContainer.jsx
+++ src/components/SignInContainer/SignInContainer.jsx
@@ -1,62 +1,99 @@
-import { useCallback } from "react";
-import TextField from "../TextField/TextField";
+import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FrameComponent from "../FrameComponent/FrameComponent";
 import "./SignInContainer.css";
+
 const SignInContainer = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const onButtonContainerClick = useCallback(() => {
-    navigate("/");
-  }, [navigate]);
+    // Handle sign in logic
+    console.log("Sign in clicked");
+  }, []);
 
   const onSignUpText1Click = useCallback(() => {
     navigate("/signup-mail");
   }, [navigate]);
 
+  const handleEmailChange = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const handlePasswordChange = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+  }, []);
+
   return (
     <div className="sign-in-container">
       <div className="sign-in2">Sign in</div>
       <div className="sign-in-form">
-        <TextField
-          label="User name or email address"
-          icon="/icon1.svg"
-          prop="designer@gmail.com"
-          icons="/icons.svg"
-          textFieldWidth="35.5rem"
-          propDisplay="none"
-        />
-        <div className="password-form1">
-          <TextField
-            label="Your password"
-            icon="/icon1.svg"
-            prop="123456"
-            icons="/icons.svg"
-            textFieldWidth="35.5rem"
-            rectangle1Display="unset"
-          />
-          <div className="forget-your-password">Forget your password</div>
-        </div>
-        <div className="button-container">
-          <div className="button2" onClick={onButtonContainerClick}>
-            <div className="icons-container">
-              <img className="icons4" alt="" src="/icons2.svg" />
-              <div className="sign-up4">Sign in</div>
-            </div>
-          </div>
-          <div className="sign-up-redirect1">
-            <div className="text2">
-              <div className="dont-have-an-container1">
-                <span className="dont-have-an1">Don’t have an account?</span>
-                <span className="span3">{` `}</span>
-                <span className="span4">{`  `}</span>
-              </div>
-              <div className="sign-up5" onClick={onSignUpText1Click}>
-                Sign up
+        <form onSubmit={handleSubmit}>
+          <div className="text-field2">
+            <div className="label-group">
+              <div className="label1">User name or email address</div>
+              <div className="password-hide-see1" style={{ display: "none" }}>
+                <img className="icon1" alt="" src="/icon1.svg" />
+                <div className="hide1">Hide</div>
               </div>
             </div>
+            <input
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              className="div7"
+            />
+            <div className="error-message1">Error message</div>
           </div>
-        </div>
+
+          <div className="password-form1">
+            <div className="text-field2">
+              <div className="label-group">
+                <div className="label1">Your password</div>
+                <div className="password-hide-see1">
+                  <img className="icon1" alt="" src="/icon1.svg" />
+                  <div className="hide1">Hide</div>
+                </div>
+              </div>
+              <input
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="div7"
+              />
+              <div className="error-message1">Error message</div>
+            </div>
+            <div className="forget-your-password">Forget your password</div>
+          </div>
+
+          <div className="button-container">
+            <button type="submit" className="button2" onClick={onButtonContainerClick}>
+              <div className="icons-container">
+                <img className="icons4" alt="" src="/icons2.svg" />
+                <div className="sign-up4">Sign in</div>
+              </div>
+            </button>
+            <div className="sign-up-redirect1">
+              <div className="text2">
+                <div className="dont-have-an-container1">
+                  <span className="dont-have-an1">Don’t have an account?</span>
+                  <span className="span3">{` `}</span>
+                  <span className="span4">{`  `}</span>
+                </div>
+                <div className="sign-up5" onClick={onSignUpText1Click}>
+                  Sign up
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
       <div className="divider3">
         <div className="divider4" />
