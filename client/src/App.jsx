e4bc56f76a5cb5a3b33b0216568320075867d7be
AazimAnish <aazimanish1@gmail.com>
2023-06-05 18:06:09 +0530

    circle create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,8 @@ import SignUpMail from "./pages/SignUpMail/SignUpMail";
 import Login from "./pages/login/login";
 import SignUpPassword from "./pages/SignUpPassword/SignUpPassword";
 import { useEffect } from "react";
-import HomeCircle from "./pages/learningCircle/homeCircle/homeCircle";
+import CreateCircle from "./pages/learningCircle/createCircle/createCircle";
+
 
 function App() {
   const location = useLocation();
@@ -73,7 +74,7 @@ function App() {
       <Route path="/signup-mail" element={<SignUpMail />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup-password" element={<SignUpPassword />} />
-      <Route path="/circle-home" element={<HomeCircle />} />
+      <Route path="/create-circle" element={<CreateCircle />} />
     </Routes>
     
   );
